7b9ba74bf92c12a77ebfba51385cb6a7a7dc5170
HyunBin Kim <hbkim0730@gmail.com>
2014-10-04 00:21:44 -0700

    Added new model files

---
+++ Boba-ios/BobaApp/BobaApp/Model/BABobaRun.m
@@ -0,0 +1,13 @@
+//
+//  BABobaRun.m
+//  BobaApp
+//
+//  Created by Hyun Bin Kim on 2014-10-04.
+//  Copyright (c) 2014 coursera interns. All rights reserved.
+//
+
+#import "BABobaRun.h"
+
+@implementation BABobaRun
+
+@end
---
+++ Boba-ios/BobaApp/BobaApp/Model/BAPeople.h
@@ -0,0 +1,13 @@
+//
+//  BAPeople.h
+//  BobaApp
+//
+//  Created by Hyun Bin Kim on 2014-10-04.
+//  Copyright (c) 2014 coursera interns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BAPeople : NSObject
+
+@end
