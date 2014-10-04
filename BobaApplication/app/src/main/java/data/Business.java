20af51e294e70aa7c2210dafaf725b80e0cdeb3c
47mspa <michelle_wong47@hotmail.com>
2014-10-04 02:00:38 -0700

    adding yelp support

---
+++ BobaApplication/app/src/main/java/data/Business.java
@@ -0,0 +1,12 @@
+package data;
+
+public class Business {
+    public final String name;
+    public final String phone;
+
+    public Business(String name,
+                    String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
