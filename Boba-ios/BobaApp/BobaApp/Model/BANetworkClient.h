421896943b0e156ab637834b099a1330e0694024
HyunBin Kim <hbkim0730@gmail.com>
2014-10-04 03:17:34 -0700

    completed network stuff

--- Boba-ios/BobaApp/BobaApp/Model/BANetworkClient.h
+++ Boba-ios/BobaApp/BobaApp/Model/BANetworkClient.h
@@ -8,6 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+
+typedef void (^PAHTTPClientSuccess)(id responseObject);
+typedef void (^PAHTTPClientFailure)(NSError *error);
+
 @interface BANetworkClient : NSObject
++ (id)sharedClient;
+
+#pragma mark - User
+- (void)registerNewUserWithUseremail:(NSString *)userEmail phoneNumber:(NSString *)phoneNumber success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)getUserDetailsWithUserEmail:(NSString *)userEmail success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+
+#pragma mark - Group
+- (void)createNewGroupWithGroupName:(NSString *)groupName groupPassword:(NSString *)groupPassword success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)addNewGroupMemberWithGroupName:(NSString *)groupName userEmail:(NSString *)userEmail success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+
+#pragma mark - Run
+- (void)getCurrentActiveRunsWithGroupName:(NSString *)groupName success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)createNewRunWithGroupName:(NSString *)groupName runName:(NSString *)runName yelpId:(NSString *)yelpId orderTime:(long)orderTime expireTime:(long)expireTime success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)getRunStatusWithGroupName:(NSString *)groupName runName:(NSString *)runName success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)addUserToRunWithUserEmail:(NSString *)userEmail GroupName:(NSString *)groupName runName:(NSString *)runName success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)markRunCompletedWithGroupName:(NSString *)groupName runName:(NSString *)runName success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
+- (void)markUserPaidWithUserEmail:(NSString *)userEmail groupName:(NSString *)groupName runName:(NSString *)runName success:(PAHTTPClientSuccess)success failure:(PAHTTPClientFailure)failure;
 
 @end
