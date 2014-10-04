810f47695b8bcae8936764584aaa60dee74fe26e
Marc Poulin <marc.poulin@uwaterloo.ca>
2014-10-04 01:09:48 -0700

    Hello, world

--- boba_app_node/server.js
+++ boba_app_node/server.js
@@ -1,1 +1,13 @@
 var express = require('express');
+
+var app = express();
+
+app.get('/api', function (req, res) {
+  res.send('it works~');
+});
+
+// Launch server
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
